a65b4eae887b6eaa6a866bf6c69a8445312f258c
Isai Torres <142456446+isai-torres@users.noreply.github.com>
2024-09-26 13:40:00 -0400

    update actividad clase libro 05 and create actividad clase estudiante 06

---
+++ Exercises/actividad_clase_estudiante_06/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace actividad_clase_estudiante_06 {
+
+  // Crear clase Estudiante
+  public class Estudiante {
+
+    // Atributos
+    public string nombre {get; set;}
+    public int edad {get; set;}
+    public string concentracion {get; set;}
+    public float clasificacionPromedio {get; set;}
+
+    // Método para mostrar Detalles
+
+    public void MostrarDetalles() {
+      Console.WriteLine($"1. Nombre: {nombre} \n2. Edad: {edad} \n3. Concentración: {concentracion} \n4. Clasificación {clasificacionPromedio}");
+    }
+  }
+  internal class Program {
+    private static void Main(string[] args)
+    {
+      // Creación de los objetos
+
+      Estudiante estudiante = new Estudiante();
+      estudiante.nombre = "Pedro";
+      estudiante.edad = 25;
+      estudiante.concentracion = "Ciencias de Computadora";
+      estudiante.clasificacionPromedio = 80.45f;
+
+      Estudiante estudiante2 = new Estudiante();
+      estudiante2.nombre = "Juan";
+      estudiante2.edad = 22;
+      estudiante2.concentracion = "Ciencias de Computadora";
+      estudiante2.clasificacionPromedio = 60.52f;
+
+      Estudiante estudiante3 = new Estudiante();
+      estudiante3.nombre = "Karla";
+      estudiante3.edad = 23;
+      estudiante3.concentracion = "Biología";
+      estudiante3.clasificacionPromedio = 90.49f;
+
+      // Mostrar detalles de los estudiantes
+
+      Console.WriteLine("Mostrar detalles del estudiante 1");
+      Console.WriteLine();
+      estudiante.MostrarDetalles();
+      Console.WriteLine();
+
+      Console.WriteLine("Mostrar detalles del estudiante 2");
+      Console.WriteLine();
+      estudiante2.MostrarDetalles();
+      Console.WriteLine();
+
+      Console.WriteLine("Mostrar detalles del estudiante 3");
+      Console.WriteLine();
+      estudiante3.MostrarDetalles();
+      Console.WriteLine();
+
+      // Modificación del valor de un atributo de los objetos
+      estudiante3.concentracion = "Ciencias de Computadoras";
+      Console.WriteLine("Karla cambio de concentracion de Biología a Ciencias de Computadoras.");
+      Console.WriteLine();
+      estudiante3.MostrarDetalles();
+    }
+  }
+}
--- Exercises/actividad_clase_libro_05/Program.cs
+++ Exercises/actividad_clase_libro_05/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 
 namespace actividad_clase_libro_05 {
-  class Libro {
+
+  // Se crea la clase
+  public class Libro {
+
+    // Atributos
     public string titulo {get; set;}
     public string autor {get; set;}
     public string genero {get; set;}
     public int numPaginas {get; set;}
 
+    // Método para mostrar detalles
     public void MostrarDetalles() {
       Console.WriteLine($"1. Titulo: {titulo} \n2. Autor: {autor} \n3. Genero: {genero} \n4. Total de paginas: {numPaginas}");
     }
@@ -15,12 +20,41 @@ namespace actividad_clase_libro_05 {
   class Program {
     private static void Main(string[] args) {
 
+      // Creación de los objetos
+      
       Libro libro = new Libro();
       libro.titulo = "Meditations";
       libro.autor = "Marcus Aurelius";
       libro.genero = "Filosofía";
       libro.numPaginas = 450;
+
+      Libro libro2 = new Libro();
+      libro2.titulo = "El Principito";
+      libro2.autor = "Antoine";
+      libro2.genero = "Novela";
+      libro2.numPaginas = 120;
+
+      Libro libro3 = new Libro();
+      libro3.titulo = "Python";
+      libro3.autor = "Robert Lafore";
+      libro3.genero = "Programming";
+      libro3.numPaginas = 1040;
+
+      // Mostrar detalles de los libros
+
+      Console.WriteLine("Detalles del libro 1: ");
+      Console.WriteLine();
       libro.MostrarDetalles();
+      Console.WriteLine(); // Esto es para crear un espacio en blanco
+
+      Console.WriteLine("Detalles del libro 2: ");
+      Console.WriteLine();
+      libro2.MostrarDetalles();
+      Console.WriteLine(); // Esto es para crear un espacio en blanco
+
+      Console.WriteLine("Detalles del libro 3: ");
+      Console.WriteLine();
+      libro3.MostrarDetalles();
     }
   }
 }
