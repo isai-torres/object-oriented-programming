8e3a09383939ae86e5579330b0874f4d07ce3509
Isai Torres <142456446+isai-torres@users.noreply.github.com>
2024-10-30 11:33:51 -0400

    added encapsulamiento_producto_11

---
+++ Exercises/encapsulamiento_producto_11/ClassProducto.cs
@@ -0,0 +1,6 @@
+class Producto {
+  private string nombre {get; set;}
+  private double salario {get; set;}
+
+  
+}
---
+++ Exercises/encapsulamiento_producto_11/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace encapsulamiento_producto_11 {
+  class Program {
+    private static void Main(string[] args) {
+      
+    }
+  }
+}
