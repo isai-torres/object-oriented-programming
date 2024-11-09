719e332e721968bfcd0a2c2cdc8f901cee4e7af8
Isai Torres <142456446+isai-torres@users.noreply.github.com>
2024-11-09 13:52:10 -0400

    added metodo_overloading_14

---
+++ Exercises/metodo_overloading_14/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace metodo_overloading_14 {
+  class Program {
+    private static void Main(string[] args) {
+      // Crear las instancias
+      Calculadora calculadora = new Calculadora();
+
+      // Obtener los valores de los parámetros y mostrarlos
+      Console.WriteLine(calculadora.sumar(8, 8));
+      Console.WriteLine(calculadora.sumar(20.10, 20.10));
+      Console.WriteLine(calculadora.sumar(5, 5, 5));
+    }
+  }
+}
---
+++ Exercises/metodo_overloading_14/classCalculadora.cs
@@ -0,0 +1,14 @@
+class Calculadora {
+  // Sobrecarga de métodos para sumar diferentes tipos de datos
+  public int sumar(int a, int b){
+    return a + b;
+  }
+
+  public double sumar(double a, double b) {
+    return a + b;
+  }
+
+  public int sumar(int a, int b, int c) {
+    return a + b + c;
+  }
+}
