b32c617e9c31497f200d90e37f8a9226d15e05a9
Isai Torres <142456446+isai-torres@users.noreply.github.com>
2024-11-09 16:06:07 -0400

    added actividad_calculadora_simple_15

---
+++ Exercises/actividad_calculadora_simple_15/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace actividad_calculadora_simple_15 {
+  class Program {
+    private static void Main(string[] args) {
+      // Crear instancias
+      CalculadoraSimple miCalculadoraSimple = new CalculadoraSimple();
+
+      // Mostrar los resultados
+      Console.WriteLine("Multiplicación de dos enteros: " + miCalculadoraSimple.Multiplicar(4, 2));
+      Console.WriteLine("Multiplicación de dos doubles: " + miCalculadoraSimple.Multiplicar(2.44, 2.44));
+    }
+  }
+}
---
+++ Exercises/actividad_calculadora_simple_15/classCalculadoraSimple.cs
@@ -0,0 +1,10 @@
+class CalculadoraSimple {
+  // Crear métodos
+  public int Multiplicar(int a, int b) {
+    return a * b;
+  }
+
+  public double Multiplicar(double a, double b) {
+    return a * b;
+  }
+}
