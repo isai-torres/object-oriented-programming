dd70c6fc88d77fbbc585db56f00d357ab748bbc6
Isai Torres <142456446+isai-torres@users.noreply.github.com>
2024-11-10 13:23:38 -0400

    added actividad_operacion_16

---
+++ Exercises/actividad_operacion_16/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace actividad_operacion_16 {
+  class Program {
+    private static void Main(string[] args) {
+      // Crear las instancias
+      Operacion operacionMatematica = new Operacion();
+
+      // Mostrar los resultados y asignar el valor de los parámetros
+      Console.WriteLine("La suma de los dos números enteros es: " + operacionMatematica.calcular(7, 7));
+      Console.WriteLine("La multiplicadion de los tres doubles es: " + operacionMatematica.calcular(2.2, 3.4, 5.6));
+    }
+  }
+}
---
+++ Exercises/actividad_operacion_16/classOperacion.cs
@@ -0,0 +1,11 @@
+class Operacion {
+  
+  // Crear el concepto del método overloading
+  public int calcular(int a, int b) {
+    return a + b;
+  }
+
+  public double calcular(double a, double b, double c) {
+    return a * b * c;
+  }
+}
